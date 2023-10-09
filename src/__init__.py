87ebcd5fd111d8fc87e385d8c8b38ec9161d6eef
ranjian0 <karanjaichungwa@gmail.com>
2023-10-09 18:40:24 +0300

    bl_info sample

---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+bl_info = {
+    "name": "Blender Addon Name",
+    "description": "Some description for the addon",
+    "author": "Author Name",
+    "version": (0, 0, 1),
+    "blender": (3, 3, 0),
+    "location": "View3D", # change me
+    "warning": "",
+    "wiki_url": "",
+    "category": "Import-Export" # change me
+}
+
+def register() -> None:
+    pass
+
+
+def unregister() -> None:
+    pass
